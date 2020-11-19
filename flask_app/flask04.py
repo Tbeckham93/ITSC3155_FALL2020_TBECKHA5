3de0c612d2bc310e150581ce02e0ee396c852fef
Timothy Beckham <tbeckha5@uncc.edu>
2020-11-18 22:47:38 -0500

    flask app #4 - db queries

--- flask_app/flask04.py
+++ flask_app/flask04.py
@@ -7,6 +7,8 @@ from flask import render_template
 from flask import request
 from flask import redirect, url_for
 from database import db
+from models import Note as Note
+from models import User as User
 
 app = Flask(__name__)  # create an app
 
@@ -20,7 +22,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 # Setup models
 with app.app_context():
-    db.create_all()   # run under the app context
+    db.create_all()  # run under the app context
 
 
 # @app.route is a decorator. It gives the function "index" special powers.
@@ -29,36 +31,40 @@ with app.app_context():
 @app.route('/')
 @app.route('/index')
 def index():
-    a_user = {'name': 'Timothy', 'email': 'tbeckha5@uncc.edu'}
+    a_user = db.session.query(User).filter_by(email='tbeckha5@uncc.edu')
     return render_template('index.html', user=a_user)
 
 
 @app.route('/notes')
 def get_notes():
-    a_user = {'name': 'Timothy', 'email': 'tbeckha5@uncc.edu'}
-    return render_template('notes.html', notes=notes, user=a_user)
+    a_user = db.session.query(User).filter_by(email='tbeckha5@uncc.edu')
+    my_notes = db.session.query(Note).all()
+    return render_template('notes.html', notes=my_notes, user=a_user)
 
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
-    a_user = {'name': 'Timothy', 'email': 'tbeckha5@uncc.edu'}
-    return render_template('note.html', note=notes[int(note_id)], user=a_user)
+    a_user = db.session.query(User).filter_by(email='tbeckha5@uncc.edu')
+    my_note = db.session.query(Note).filter_by(id=note_id)
+    return render_template('note.html', note=my_note, user=a_user)
 
 
 @app.route('/notes/new', methods=['GET', 'POST'])
 def new_notes():
     print('request method is', request.method)
-    a_user = {'name': 'Timothy', 'email': 'tbeckha5@uncc.edu'}
+    # a_user = db.session.query(User).filter_by(email='tbeckha5@uncc.edu')
     if request.method == 'POST':
         title = request.form['title']
         text = request.form['noteText']
         from datetime import date
         today = date.today()
         today = today.strftime("%m-%d-%Y")
-        id = len(notes)+1
-        notes[id] = {'title': title, 'text': text, 'date': today}
-        return redirect(url_for('get_notes', name=a_user))
+        newEntry = Note(title, text, today)
+        db.session.add(newEntry)
+        db.session.commit()
+        return redirect(url_for('get_notes'))
     else:
+        a_user = db.session.query(User).filter_by(email='tbeckha5@uncc.edu')
         return render_template('new.html', user=a_user)
 
 
